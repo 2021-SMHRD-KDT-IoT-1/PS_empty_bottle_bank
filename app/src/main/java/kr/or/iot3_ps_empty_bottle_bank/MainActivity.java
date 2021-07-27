f2856720593d86a2121521c5558c812bbaca999d
spring41 <rufn41@gmail.com>
2021-07-27 17:41:40 +0900

    Initial commit

---
+++ app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.or.iot3_ps_empty_bottle_bank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
