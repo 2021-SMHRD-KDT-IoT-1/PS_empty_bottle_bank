6772784be1e7b650a95f7da54a54e9dfa6306532
Iot3cha <cjh19960923@nate.com>
2021-07-28 17:46:01 +0900

    정현 fragment 깔아놓음

--- app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/MainActivity.java
+++ app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/MainActivity.java
@@ -1,16 +1,59 @@
 package kr.or.iot3_ps_empty_bottle_bank;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigation;
+    Fragment_Main fragment_main;
+    Fragment_Event fragment_event;
+    Fragment_Guide fragment_guide;
+    Fragment_Myinfo fragment_myinfo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //테스트 진석진석진석 내 이름은 진석 콩콩콩진석
+        navigation = findViewById(R.id.navigation);
+        fragment_main = new Fragment_Main();
+        fragment_event = new Fragment_Event();
+        fragment_guide = new Fragment_Guide();
+        fragment_myinfo = new Fragment_Myinfo();
+
+
+        //프래그먼트매니저를 이용하여 FrameLayout에 Fragment화면을 설정
+        //replace(부모레이아웃, 화면에 출력할 프래그먼트객체)
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment_main).commit();
+
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if(itemId == R.id.item_main){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment_main).commit();
+                }else if (itemId == R.id.item_event){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment_event).commit();
+                }else if (itemId == R.id.item_guide){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment_guide).commit();
+                }else if (itemId == R.id.item_myinfo){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment_myinfo).commit();
+                }
+
+                //true : 클릭한 메뉴아이템에 포커싱
+                return true;
+            }
+        });
+
     }
 }
