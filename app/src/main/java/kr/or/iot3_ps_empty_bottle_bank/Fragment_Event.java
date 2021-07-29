146aaecde21a98ce5990b397df3c573654bd5e0d
Iot3cha <cjh19960923@nate.com>
2021-07-29 09:12:54 +0900

    정현 네비 파일 업데이트

---
+++ app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/Fragment_Event.java
@@ -0,0 +1,23 @@
+package kr.or.iot3_ps_empty_bottle_bank;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_Event extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_event, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/Fragment_Guide.java
@@ -0,0 +1,25 @@
+package kr.or.iot3_ps_empty_bottle_bank;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment_Guide extends Fragment {
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_guide, container, false);
+
+        return view;
+    }
+}
