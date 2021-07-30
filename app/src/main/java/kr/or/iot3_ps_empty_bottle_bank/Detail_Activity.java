e8ffb52df1af3aaa4f3b9de8df838da92c9934b2
Hong-02 <wlstjr2626@naver.com>
2021-07-30 11:01:36 +0900

    Hong - 오늘도 좋은 아침입니다. 리스트뷰 목록 상세페이지 제작 및 , 지도 API들어갈 페이지 제작 PS공병 화이팅!

--- app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/Detail_Activity.java
+++ app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/Detail_Activity.java
@@ -2,13 +2,45 @@ package kr.or.iot3_ps_empty_bottle_bank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Detail_Activity extends AppCompatActivity {
+
+    Button Detail_btn_back;
+    Button Check_loc_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_page);
+        setContentView(R.layout.activity_detail);
+
+        // 뒤로가기
+        Detail_btn_back = findViewById(R.id.Detail_btn_back);
+        // 위치보기 == 여기에 API 기능 넣어줘야 함
+        Check_loc_btn = findViewById(R.id.Check_loc_btn);
+
+
+        Detail_btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Fragment_Main.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Check_loc_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Check_Loc_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
