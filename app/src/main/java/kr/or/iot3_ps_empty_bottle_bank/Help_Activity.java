7128b4c59a2ce4e74e83897969f58303fda7ec9a
Iot3cha <cjh19960923@nate.com>
2021-08-02 17:57:57 +0900

    오후 17:57 , 최정현  0802

---
+++ app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/Help_Activity.java
@@ -0,0 +1,16 @@
+package kr.or.iot3_ps_empty_bottle_bank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Help_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+
+    }
+}
