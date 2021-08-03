68ff4678f5873cbcce869fcb5adc43b3ca9d0053
Iot3cha <cjh19960923@nate.com>
2021-08-03 16:57:48 +0900

    오후 17:57 , 최정현  0802

--- app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/Check_Loc_Activity.java
+++ app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/Check_Loc_Activity.java
@@ -1,8 +1,16 @@
 package kr.or.iot3_ps_empty_bottle_bank;
 
+import android.os.Bundle;
+import android.view.ViewGroup;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
+import net.daum.mf.map.api.MapView;
+
+
+
+
 
 public class Check_Loc_Activity extends AppCompatActivity {
 
@@ -10,5 +18,17 @@ public class Check_Loc_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_loc);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
+
+
+
+
 }
+
+
+
