70ecd3eccb8c2b77b2ac1b066309768db9d8203d
Hong-02 <wlstjr2626@naver.com>
2021-08-04 12:03:20 +0900

    Hong - 08.04 PM12:00 로그인 회원가입 서버연동 및 메인페이지 광고노출 완성 프젝 꼭 1등 합시다!! PS공병팀 화이팅!!

--- app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/QR_main_Activity.java
+++ app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/QR_main_Activity.java
@@ -2,6 +2,7 @@ package kr.or.iot3_ps_empty_bottle_bank;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -19,61 +20,37 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.journeyapps.barcodescanner.CaptureActivity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class QR_main_Activity extends AppCompatActivity {
 
-    WebView QR_wv;
-    EditText QR_et;
-    Button QR_bt;
     IntentIntegrator integrator;
+    RequestQueue queue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_main);
 
-
-        QR_wv = findViewById(R.id.QR_wv);
-        QR_et = findViewById(R.id.QR_et);
-
-        QR_bt = findViewById(R.id.QR_bt);
-        WebSettings webSettings = QR_wv.getSettings();
-
-        //자바 스크립트 사용을 할 수 있도록 합니다.
-        webSettings.setJavaScriptEnabled(true);
-
-        //페이지 로딩이 끝나면 호출됩니다.
-        QR_wv.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                Toast.makeText(QR_main_Activity.this,"로딩 완료!!",Toast.LENGTH_SHORT).show();
-            }
-        });
-        QR_et.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH){
-                    //bt의 onClick을 실행
-                    QR_bt.callOnClick();
-                    // 키보드 숨기기
-                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(v.getWindowToken(),0);
-                    return true;
-                }
-
-                return false;
-            }
-        });
-
+        queue = Volley.newRequestQueue(getApplicationContext());
         integrator = new IntentIntegrator(this);
-
+        integrator.setOrientationLocked(false);
 
         // 바코드 안의 텍스트
-        integrator.setPrompt("바코드를 사각형 안에 비춰주세요");
 
         // 바코드 인식 시 소리 여부
         integrator.setBeepEnabled(false);
@@ -88,56 +65,59 @@ public class QR_main_Activity extends AppCompatActivity {
 
     }
 
-    public void OnClick(View view) {
-        String address = QR_et.getText().toString();
-
-        if (!address.startsWith("http://")) {
-            address = "http://" + address;
-        }
-        QR_wv.loadUrl(address);
-
-    Log.d("QR주소", address);
-
-
-    }
-
     @Override
-    public void onBackPressed() {
-        if (QR_wv.isActivated()) {
-            if (QR_wv.canGoBack()) {
-                QR_wv.goBack();
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (result != null) {
+            if (result.getContents() == null) {
+
             } else {
-                //스캐너 시작
-                integrator.initiateScan();
+                // qr코드 가져옴
+
+
+                //  회수기 정보가 같이 보내져야함
+                String login_url = result.getContents();
+                Log.d("mmmmm", login_url);
+
+
+                StringRequest request = new StringRequest(Request.Method.POST, login_url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                    Toast.makeText(getApplicationContext(), "접속 성공", Toast.LENGTH_SHORT).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(), "접속 실패", Toast.LENGTH_SHORT).show();
+                            }
+                        }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+
+//                        params.put("id", id);
+//                        params.put("pw", pw);
+
+                        return params;
+                    }
+                };
+                queue.add(request);
+
+
+
+                // 화면 전환
+//                Intent intent = new Intent(getApplicationContext(), Detail_Activity.class);
+//                startActivity(intent);
             }
         } else {
-            super.onBackPressed();
+            super.onActivityResult(requestCode, resultCode, data);
         }
+
     }
-
-
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//
-//        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-//
-//        if (result != null) {
-//            if (result.getContents() == null) {
-//
-//            } else {
-//                //qr코드를 읽어서 Edit Text에 입력해줍니다.
-//                QR_et.setText(result.getContents());
-//
-//                //Button의 onclick호출
-//                QR_bt.callOnClick();
-//
-//                Toast.makeText(this, "Scanned :" + result.getContents(), Toast.LENGTH_SHORT).show();
-//            }
-//        } else {
-//            super.onActivityResult(requestCode, resultCode, data);
-//        }
-//
-//    }
 
 
 }
