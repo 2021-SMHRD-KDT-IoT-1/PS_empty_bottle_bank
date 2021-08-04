93e1200e9bcd8a054e291929083137f7e2d99775
Iot3cha <cjh19960923@nate.com>
2021-08-04 12:26:41 +0900

    오후 12:26 , 최정현  0804

--- app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/Check_Loc_Activity.java
+++ app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/Check_Loc_Activity.java
@@ -1,29 +1,255 @@
 package kr.or.iot3_ps_empty_bottle_bank;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapReverseGeoCoder;
 import net.daum.mf.map.api.MapView;
 
 
 
-public class Check_Loc_Activity extends AppCompatActivity {
+
+public class Check_Loc_Activity extends AppCompatActivity implements
+    MapView.CurrentLocationEventListener, MapReverseGeoCoder.ReverseGeoCodingResultListener {
+
+    private static final String LOG_TAG = "Check_Loc_Activity";
+
+    private MapView mMapView;
+
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION};
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_loc);
 
-        MapView mapView = new MapView(this);
+       mMapView = (MapView) findViewById(R.id.map_view);
 
-        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
+       mMapView.setCurrentLocationEventListener(this);
+
+       if(!checkLocationServicesStatus()){
+
+           showDialogForLocationServiceSetting();
+       }else {
+
+           checkRunTimePermission();
+       }
 
 
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        mMapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOff);
+        mMapView.setShowCurrentLocationMarker(false);
+    }
+
+    @Override
+    public void onCurrentLocationUpdate(MapView mapView, MapPoint currentLocation, float accuracyInMeters){
+        MapPoint.GeoCoordinate mapPointGeo = currentLocation.getMapPointGeoCoord();
+        Log.i(LOG_TAG, String.format("MapView onCurrentLocationUpdate (%f,%f) accuracy (%f)",
+                mapPointGeo.latitude, mapPointGeo.longitude, accuracyInMeters));
+    }
+
+
+    @Override
+    public void onCurrentLocationDeviceHeadingUpdate(MapView mapView, float v) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateFailed(MapView mapView) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateCancelled(MapView mapView) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void onReverseGeoCoderFoundAddress(MapReverseGeoCoder mapReverseGeoCoder, String s) {
+        mapReverseGeoCoder.toString();
+        onFinishReverseGeoCoding(s);
+    }
+
+
+
+    @Override
+    public void onReverseGeoCoderFailedToFindAddress(MapReverseGeoCoder mapReverseGeoCoder) {
+        onFinishReverseGeoCoding("Fail");
+    }
+
+    private void onFinishReverseGeoCoding(String result) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grandResults) {
+
+        super.onRequestPermissionsResult(permsRequestCode, permissions, grandResults);
+        if (permsRequestCode == PERMISSION_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+
+            boolean check_result = true;
+
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+
+            if (check_result) {
+                Log.d("@@", "start");
+
+                mMapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
+
+            } else {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
+                    Toast.makeText(Check_Loc_Activity.this, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.",
+                            Toast.LENGTH_LONG).show();
+                    finish();
+
+                } else {
+
+                    Toast.makeText(Check_Loc_Activity.this, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
+                            Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+
+
+        }
+
+    }
+
+
+    void checkRunTimePermission() {
+
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(Check_Loc_Activity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED) {
+
+            mMapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
+
+        }else{
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(Check_Loc_Activity.this,REQUIRED_PERMISSIONS[0])) {
+
+                Toast.makeText(Check_Loc_Activity.this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+
+                ActivityCompat.requestPermissions(Check_Loc_Activity.this, REQUIRED_PERMISSIONS, PERMISSION_REQUEST_CODE);
+
+
+            } else {
+
+                ActivityCompat.requestPermissions(Check_Loc_Activity.this, REQUIRED_PERMISSIONS, PERMISSION_REQUEST_CODE);
+
+
+            }
+
+        }
+
+    }
+
+
+
+    private void showDialogForLocationServiceSetting() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Check_Loc_Activity.this);
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n" + "위치 설정을 수정하시겠습니까?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Intent callGPSSettingIntent
+                        = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+
+        builder.create().show();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+
+            case GPS_ENABLE_REQUEST_CODE:
+
+                if (checkLocationServicesStatus()) {
+                    if (checkLocationServicesStatus()){
+
+                        Log.d("@@@", "onActivityResult : GPS활성화 돼있음");
+                        checkRunTimePermission();
+                        return;
+
+                    }
+                }
+
+                break;
+
+        }
+
+    }
+
+
+
+    private boolean checkLocationServicesStatus() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
     }
 
 
