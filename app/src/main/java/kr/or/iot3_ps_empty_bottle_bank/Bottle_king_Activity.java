542d7880ec06d9c88eeb4e894364ceefd2874950
Iot3cha <cjh19960923@nate.com>
2021-08-05 11:02:07 +0900

    오전 11:01 , 최정현  0805

--- app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/Bottle_king_Activity.java
+++ app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/Bottle_king_Activity.java
@@ -3,7 +3,11 @@ package kr.or.iot3_ps_empty_bottle_bank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 
@@ -21,7 +25,7 @@ public class Bottle_king_Activity extends AppCompatActivity {
 
 
 
-    private int[] bottleArray = {R.drawable.sojo, R.drawable.apeach, R.drawable.item1, R.drawable.item2,R.drawable.item3};
+    private int[] bottle_img_Array = {R.drawable.sojo, R.drawable.apeach, R.drawable.item1, R.drawable.item2,R.drawable.item3};
 
     private String[] bottle_round = {"1", "2", "3" ,"4","5"};
     private String[] bottle_id = {"홍진석", "콩진석" , "콩콩진석", "콩콩콩진석", "콩콩콩콩진석"};
@@ -33,16 +37,48 @@ public class Bottle_king_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottle_king);
 
+
+        String event_num = getIntent().getStringExtra("event_num");
+        Log.d("이거 오류얌", event_num);
+
+
+
+
+
+        // 여기쯤에서 정보 가져와야함
+
+
+        JSONArray lll = new JSONArray();
+
+
+
+
         ListView bottle_list_king = findViewById(R.id.bottle_list_king);
         bottle_king_data = new ArrayList<>();
 
-        for(int i = 0; i<bottleArray.length; i++) {
-            bottle_king_data.add(new bottle_kongVO(bottleArray[i], bottle_round[i], bottle_id[i],bottle_now[i],bottle_score[i]));
+
+
+        for(int i = 0; i<bottle_img_Array.length; i++) {
+
+
+            bottle_king_data.add(new bottle_kongVO(
+
+                   /* machine_name = machine.getString("machine_name");    <----- 이게 JSON형식 */
+
+            /*JSON가져오기 */bottle_img_Array[i],
+                    bottle_round[i],
+                    bottle_id[i],bottle_now[i],bottle_score[i]));
         }
 
 
         bottle_king_Adapter = new bottle_king_Adapter(getApplicationContext(), R.layout.bottle_king_win, bottle_king_data);
         bottle_list_king.setAdapter(bottle_king_Adapter);
 
+
+
+
+
+
+         ;
     }
 }
