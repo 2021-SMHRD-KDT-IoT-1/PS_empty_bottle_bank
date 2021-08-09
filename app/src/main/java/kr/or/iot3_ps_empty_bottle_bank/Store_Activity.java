34ade9c318cf5c7ee08ee9e881db75cc5a02737e
Hong-02 <wlstjr2626@naver.com>
2021-08-09 17:48:57 +0900

    Hong - 08.09 PM5:48

--- app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/Store_Activity.java
+++ app/src/main/java/kr/or/iot3_ps_empty_bottle_bank/Store_Activity.java
@@ -1,13 +1,19 @@
 package kr.or.iot3_ps_empty_bottle_bank;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -35,6 +41,7 @@ public class Store_Activity extends AppCompatActivity {
     private PSbankAdapter.Store_product_Adapter Store_product_Adapter;
     private ArrayList<Store_productVO> product_data;
     private ArrayList<Store_s_pointVO> mypoint_data;
+    private Button btn_buy;
 
     RequestQueue queue;
 
@@ -42,9 +49,8 @@ public class Store_Activity extends AppCompatActivity {
 
 
     private int[] product_img_Array = {R.drawable.item1, R.drawable.sojo, R.drawable.apeach,
-            R.drawable.item2, R.drawable.item3,R.drawable.item3,
-            R.drawable.item3,R.drawable.item3,R.drawable.item3};
-
+            R.drawable.item2, R.drawable.item3, R.drawable.item3,
+            R.drawable.item3, R.drawable.item3, R.drawable.item3};
 
 
     @Override
@@ -52,14 +58,23 @@ public class Store_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
 
+
+        btn_buy = findViewById(R.id.btn_buy);
         queue = Volley.newRequestQueue(getApplicationContext());
         user_points = findViewById(R.id.user_points);
+
+
+
+
+
+
+
+
         // 접속한 사용자 ID가져오기===================================================
         SharedPreferences sf = getSharedPreferences("login", Context.MODE_PRIVATE);
-        String login_id = sf.getString("login_id","");
+        String login_id = sf.getString("login_id", "");
 
         String store_url = "http://rspring41.iptime.org:3000/store?id=" + login_id;
-
 
 
         request = new StringRequest(Request.Method.GET, store_url, new Response.Listener<String>() {
@@ -73,30 +88,40 @@ public class Store_Activity extends AppCompatActivity {
 
                     Log.d("나는 뭘까?", response);
 
-                    for (int i = 0; i <jsonArray.length(); i++){
-                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                        JSONArray jsonArray2 = jsonArray.getJSONArray(0);
+
+                        for (int row2 = 0; row2 < jsonArray2.length() - 1; row2++) {
+                            JSONObject jsonObject = jsonArray2.getJSONObject(row2);
+
+                            product_data.add(new Store_productVO(
+                                    jsonObject.getInt("item_code"),
+                                    product_img_Array[row2],
+                                    jsonObject.getString("item_name"),
+                                    jsonObject.getString("item_price"),
+                                    jsonObject.getString("item_count")
+
+                            ));
+
+
+                        }
+                        JSONArray jsonArray3 = jsonArray.getJSONArray(1);
+                        JSONObject userInfo = jsonArray3.getJSONObject(0);
+                        String user_point = userInfo.getString("point");
+
+                        user_points.setText(user_point);
 
 
 
 
 
 
-                        product_data.add(new Store_productVO(
-                               jsonObject.getInt("item_code"),
-                                product_img_Array[i],
-                                jsonObject.getString("item_name"),
-                                jsonObject.getString("item_price"),
-                                jsonObject.getString("item_count")
-
-                        ));
-
-                    }
 
 
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                Log.d("안녕하세요~","반갑습ㄴ다!");
+                Log.d("안녕하세요~", "반갑습ㄴ다!");
                 product_listview = findViewById(R.id.store_product_listview);
                 Store_product_Adapter = new Store_product_Adapter(getApplicationContext(), R.layout.store_buy_listview, product_data);
                 product_listview.setAdapter(Store_product_Adapter);
@@ -115,6 +140,14 @@ public class Store_Activity extends AppCompatActivity {
 
 
         queue.add(request);
+
+
+        btn_buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Store_Activity.this, "상품구매가 완료 되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
